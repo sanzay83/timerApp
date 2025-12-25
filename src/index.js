67fd3063aaff88932acdc77e73719d1be3d321a3
Sanjay Duwal <sanzay8342258@gmail.com>
2025-12-25 00:16:43 -0600

    updating the router

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import Privacypolicy from "./components/Privacypolicy";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/timerApp">
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/privacy" element={<Privacypolicy />} />
